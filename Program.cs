f333b6f93bc582c68868ceed6e3f015ff5fb2129
bayskelt <rinognom@gmail.com>
2024-03-20 08:41:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace HomeTask_2.NET
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введiть кiлькiсть грошей у Петрика: ");
+            decimal s = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Введiть цiну одного зошита: ");
+            decimal x = decimal.Parse(Console.ReadLine());
+            Console.Write("Введiть цiну одної ручки: ");
+            decimal y = decimal.Parse(Console.ReadLine());
+
+            Console.Write("\nВведiть кiлькiсть зошитiв: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Введiть кiлькiсть ручок: ");
+            int m = int.Parse(Console.ReadLine());
+
+            decimal p = x * n + y * m; /* p - сума покупки */
+
+            if (s >= p)
+            {
+                Console.Write($"\nПiсля покупки у Петрика залишиться {s - p} гривень.");
+            }
+            else
+            {
+                Console.Write("\nПетрику не вистачило грошей.");
+            }
+        }
+    }
+}
